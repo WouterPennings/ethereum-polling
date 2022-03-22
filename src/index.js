062a0683533824023eca9265eb89024bdd546d1a
WouterPennings <wouterpennings@gmail.com>
2022-03-22 15:44:35 +0100

    Js calling functions

--- src/index.js
+++ src/index.js
@@ -8,22 +8,42 @@ let abi = [
 // Following: https://docs.ethers.io/v5/single-page/#/v5/getting-started/
 const { ethers } = require("ethers");
 const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/");
-const erc20 = new ethers.Contract("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", abi, provider);
+const signer = provider.getSigner();
+const poll = new ethers.Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", abi, provider);
+const pollSigner = poll.connect(signer);
 
 async function BlockCount() {
-    console.log(await provider.getBlockNumber());
+    console.log("Blockchain: " + await provider.getBlockNumber());
 }
 
 // Also converts hex to dec
 async function Balance(addres) {
     let bal = await provider.getBalance(addres);
-    console.log(ethers.utils.formatEther(bal));
+    console.log("Balance: " + ethers.utils.formatEther(bal));
 }
 
 async function PollCurrentStandings() {
-    console.log(await erc20.currentStandings());
+    let res = await pollSigner.currentStandings();
+    console.log("Current standings: " + res);
 }
 
+async function PollVoteCount() {
+    let res = await poll.voteCount();
+    console.log("Poll Vote Count: " + res);
+}
+
+async function PollVote(vote) {
+    try {
+        await pollSigner.vote(vote);
+        console.log("Voting successfull");
+    }
+    catch (err) {
+        console.log("Voting error occured");
+    }
+}
+    
 BlockCount();
-Balance("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+//Balance("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+PollVote(true);
+PollVoteCount();
 PollCurrentStandings();
