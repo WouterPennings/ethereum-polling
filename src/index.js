52a3c0edccca2c4a651378348cbe3664c2bfe16c
WouterPennings <wouterpennings@gmail.com>
2022-03-23 11:37:37 +0100

    uknown

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const { ethers } = require("ethers");
 function CreateThings(signer_addr, contract_addr) {
     const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/");
     const signer = provider.getSigner(signer_addr);
-    const poll = new ethers.Contract(contract_addr, abi, provider);
-    const pollSigner = poll.connect(signer);
+    const poll = new ethers.Contract(contract_addr, abi, provider); // Read only things
+    const pollSigner = poll.connect(signer); // Write things
     return [provider, signer, poll, pollSigner];
 }
 
@@ -15,17 +15,13 @@ let abi = [
     "function currentStandings() external view returns(uint)"
 ];
 
+
+
 // Following: https://docs.ethers.io/v5/single-page/#/v5/getting-started/
-const [provider, signer, poll, pollSigner] = CreateThings("0x14dC79964da2C08b23698B3D3cc7Ca32193d9955", "0x5FbDB2315678afecb367f032d93F642f64180aa3");
+const [provider, signer, poll, pollSigner] = CreateThings("0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc", "0x5FbDB2315678afecb367f032d93F642f64180aa3");
 
 async function BlockCount() {
     console.log("Blockchain: " + await provider.getBlockNumber());
-}
-
-// Also converts hex to dec
-async function Balance(addres) {
-    let bal = await provider.getBalance(addres);
-    console.log("Balance: " + ethers.utils.formatEther(bal));
 }
 
 async function PollCurrentStandings() {
@@ -49,7 +45,11 @@ async function PollVote(vote) {
 }
     
 BlockCount();
-//Balance("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-PollVote(true);
+PollVote(false);
 PollVoteCount();
-//PollCurrentStandings();
+PollCurrentStandings();
+
+// 
+
+const poll_sm = new poll_sm("0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc");
+poll_sm.vote("123", true);
