e7314e6e4cd40e7a5a7446a520f4c1b7c51a1e65
Jules <juliuszdokrzewski@gmail.com>
2022-03-24 15:38:30 +0100

    Added feature connecting with metamask wallet

--- src/components/VotePage.vue
+++ src/components/VotePage.vue
@@ -1,21 +1,38 @@
 <template>
   <div class="hello">
     <h1>Hello</h1>
+    <button class="btn btn-primary" @click="connectButtonHandler">
+      {{ connButtonText }}
+    </button>
+    <h3>{{ defaultAccount }}</h3>
   </div>
   <p></p>
 </template>
 
 <script>
-//import "../index.js";
+//TODO: make button name "connect wallet" when wallet is not connected else "wallet connected"
+//TODO: display the account hash when wallet connected
 export default {
   data() {
-    return {};
+    return {
+      connectButtonHandler() {
+        //Check if metamask is installed
+        if (window.ethereum) {
+          //connection with metamask
+          window.ethereum
+            .request({ method: "eth_requestAccounts" })
+            .then((result) => {
+              console.log(result[0]);
+            });
+        } else {
+          //TODO: create custom error
+          alert("you need to install metamask");
+        }
+      },
+    };
   },
 
   name: "VotePage",
-  props: {
-    msg: String,
-  },
 };
 </script>
 
